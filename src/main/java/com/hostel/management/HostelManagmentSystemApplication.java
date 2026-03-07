89b1cfb65aab4bbd4c4e05454fd8f56f4a416f1b
sanjaya <sanjaya@triesten.com>
2026-03-07 12:21:22 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/hostel/management/HostelManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.hostel.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostelManagmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostelManagmentSystemApplication.class, args);
+	}
+
+}
