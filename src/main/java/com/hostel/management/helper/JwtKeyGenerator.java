36a799bcca216c1c992f0edaef2e83d45d60fc36
sanjaya <sanjaya@triesten.com>
2026-03-08 11:12:52 +0530

    Jwt authentication implementation

--- src/main/java/com/hostel/management/helper/JwtKeyGenerator.java
+++ src/main/java/com/hostel/management/helper/JwtKeyGenerator.java
@@ -6,7 +6,7 @@ import io.jsonwebtoken.security.Keys;
 import javax.crypto.SecretKey;
 import java.util.Base64;
 
-public class JwtKwyGenerator {
+public class JwtKeyGenerator {
     public static void main(String[] args) {
         SecretKey key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
         String base64Key = Base64.getEncoder().encodeToString(key.getEncoded());
